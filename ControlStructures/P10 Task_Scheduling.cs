b06ceb2686137d62b4c70f2880222556a9022da0
Jknigel <ajknigel360@gmail.com>
2025-10-10 02:12:23 +0800

    Development 0.0.4

--- ControlStructures/P10 Task_Scheduling.cs
+++ ControlStructures/P10 Task_Scheduling.cs
@@ -2,25 +2,30 @@ public class Program10
 {
     public static void Run()
     {
-        string[] weekDays = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"];
+        string[] weekDays = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
 
-        for (int i=0; i<weekDays.Length; i++) {
-            switch(weekDays[i]) {
+        for (int i = 0; i < weekDays.Length; i++)
+        {
+            switch (weekDays[i])
+            {
                 case "Monday":
-                Console.WriteLine("Team Meeting");
-                break;
+                    Console.WriteLine("Team Meeting");
+                    break;
                 case "Tuesday":
-                Console.WriteLine("Code Review");
-                break;
+                    Console.WriteLine("Code Review");
+                    break;
                 case "Wednesday":
-                Console.WriteLine("Development");
-                break;
+                    Console.WriteLine("Development");
+                    break;
                 case "Thursday":
-                Console.WriteLine("Testing");
-                break;
+                    Console.WriteLine("Testing");
+                    break;
                 case "Friday":
-                Console.WriteLine("Deployment");
-                break;
+                    Console.WriteLine("Deployment");
+                    break;
+                default:
+                    Console.WriteLine("Rest Day");
+                    break;
             }
         }
     }
