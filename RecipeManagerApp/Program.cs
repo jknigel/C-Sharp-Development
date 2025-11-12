ca35fae7892da51bb13c4b7f3f9a4f1fdb5f30f9
jknigel <ajknigel360@gmail.com>
2025-11-12 21:43:43 +0800

    Development 0.2.3

--- RecipeManagerApp/Program.cs
+++ RecipeManagerApp/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using RecipeManagerApp;
+using RecipeManagerApp.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
--- RecipeManagerApp/Services/RecipeService.cs
+++ RecipeManagerApp/Services/RecipeService.cs
@@ -1,31 +1,27 @@
-namespace YourProjectName.Services;
-
-// We need this to know what a 'Recipe' is
-using YourProjectName.Models; 
+namespace RecipeManagerApp.Services;
+//using RecipeManagerApp.Models;
 
 public class RecipeService
 {
-    // This is the MASTER LIST. It now lives here, not in a component.
     private List<Recipe> _recipes = new() {
         new Recipe { Id = 1, Name = "Sashimi Platter", Description = "A Japanese cuisine loved by people"},
         new Recipe { Id = 2, Name = "Wagyu Beef", Description = "Delicious beef exclusively from Japan"}
     };
-    
-    // A simple counter to ensure new recipes get a unique ID
     private int _nextId = 3;
 
-    // A public method to get ALL recipes
-    public List<Recipe> GetAllRecipes()
-    {
-        return _recipes;
-    }
+    public List<Recipe> GetAllRecipes() => _recipes;
 
-    // THIS IS THE METHOD YOU NEED
-    // A public method to ADD a new recipe to the master list
-    public void AddRecipe(Recipe newRecipe)
+    // --- ADD THIS METHOD ---
+    // It finds the first recipe that matches the given id, or returns null if none is found.
+    public Recipe? GetRecipeById(int id)
     {
-        // Assign a new, unique ID before adding
-        newRecipe.Id = _nextId++;
-        _recipes.Add(newRecipe);
+        return _recipes.FirstOrDefault(r => r.Id == id);
+    }
+    // -----------------------
+    
+    public void AddRecipe(Recipe recipe)
+    {
+        recipe.Id = _nextId++;
+        _recipes.Add(recipe);
     }
 }
